9887c162a182c194d579085040b99285dcfa1061
eelcob <eelcob@hotmail.com>
2012-07-08 21:35:50 +0200

    update to 2.1 framework

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,145 +1,161 @@
-# -*- coding: utf-8 -*-
-import re
+# -*- coding: utf-8 -*
+PLUGIN_TITLE	= L('Title')
 
-###################################################################################################
+ART				= 'art-default.jpg'
+ICON			= 'icon-default.png'
+ICON_SEARCH		= 'icon-search.png'
+ICON_MORE		= 'icon-more.png'
 
-PLUGIN_TITLE = 'SBS Gemist'
-
-CHANNELS = {
-  'NET 5': {
-    'base': 'http://www.net5.nl',
-    'home': '/web/show/id=1017155',
-    'art': 'art-net5.png',
-    'icon': 'icon-net5.png'
-  },
-  'SBS 6': {
-    'base': 'http://www.sbs6.nl',
-    'home': '/web/show/id=985609',
-    'art': 'art-sbs6.png',
-    'icon': 'icon-sbs6.png'
-  },
-  'Veronica': {
-    'base': 'http://www.veronicatv.nl',
-    'home': '/web/show/id=997234',
-    'art': 'art-veronica.png',
-    'icon': 'icon-veronica.png'
-  }
-}
+PROGRAMLINK		= '/ajax/programFilter/day/0/genre/all/block/programs/range/'
+VIDEOMATCH		= Regex("(.*?)videos(.*?)")
 
 CHANNEL_ORDER      = ('NET 5', 'SBS 6', 'Veronica')
-SILVERLIGHT_PLAYER = 'http://www.plexapp.com/player/silverlight.php?stream=%s&width=%s&height=%s'
-
-# 2 programmes haven't got episodes, instead the menu item links directly to the latest episode (local 'news' and weather)
-DIFFERENT          = ('Hart van Nederland', 'Piets Weerbericht')
-
-# XPATH_PROGRAMS_PAGES is so specific to prevent capturing the wrong pagination navigation
-XPATH_PROGRAMS     = '//h3[contains(.,"Programma Gemist overzicht") or contains(.,"Programma gemist overzicht")]/parent::div/parent::div//span[@class="title"]/a[@href]'
-XPATH_EPISODES     = '//div[@class="block-large"]//div[contains(@class,"item")]'
-
-# Art and icons
-ART_DEFAULT        = 'art-default.jpg'
-ICON_DEFAULT       = 'icon-default.png'
-ICON_MORE          = 'icon-more.png'
+CHANNELS = {
+	'NET 5': {
+		'base': 'http://www.net5.nl',
+		'art': 'art-net5.png',
+		'icon': 'icon-net5.png',
+	},
+	'SBS 6': {
+		'base': 'http://www.sbs6.nl',
+		'art': 'art-sbs6.png',
+		'icon': 'icon-sbs6.png'
+	},
+	'Veronica': {
+		'base': 'http://www.veronicatv.nl',
+		'art': 'art-veronica.png',
+		'icon': 'icon-veronica.png'
+	}
+}
 
 ###################################################################################################
-
 def Start():
-  Plugin.AddPrefixHandler('/video/sbsgemist', MainMenu, PLUGIN_TITLE, ICON_DEFAULT, ART_DEFAULT)
-  Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
-  Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
+	Plugin.AddPrefixHandler('/video/sbsgemist', MainMenu, PLUGIN_TITLE, ICON, ART)
+	Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
+	Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
+	
+	ObjectContainer.title1 = PLUGIN_TITLE
+	ObjectContainer.view_group = 'List'
+	ObjectContainer.art = R(ART)	
 
-  # Set the default MediaContainer attributes
-  MediaContainer.title1    = PLUGIN_TITLE
-  MediaContainer.viewGroup = 'List'
-  MediaContainer.art       = R(ART_DEFAULT)
-
-  # Set the default cache time
-  HTTP.CacheTime = CACHE_1HOUR
-  HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16'
+	VideoClipObject.thumb = R(ICON)
+  
+	HTTP.CacheTime = CACHE_1HOUR
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:8.0) Gecko/20100101 Firefox/8.0'
 
 ###################################################################################################
-
 def MainMenu():
-  dir = MediaContainer()
 
-  for c in CHANNEL_ORDER:
-    dir.Append(Function(DirectoryItem(Programs, title=c, thumb=R(CHANNELS[c]['icon']), art=R(CHANNELS[c]['art'])), c=c))
+	oc = ObjectContainer()
 
-  return dir
+	for kanaal in CHANNEL_ORDER:
+		oc.add(DirectoryObject(key = Callback(CatagoryListing, kanaal=kanaal), title=kanaal, thumb=R(CHANNELS[kanaal]['icon']), art=R(CHANNELS[kanaal]['art'])))
 
-####################################################################################################
-
-def Programs(sender, c):
-  dir = MediaContainer(title2=c, art=R(CHANNELS[c]['art']))
-  url = CHANNELS[c]['base'] + CHANNELS[c]['home']
-
-  for programme in HTML.ElementFromURL(url, errors='ignore').xpath(XPATH_PROGRAMS):
-    title = programme.xpath('./text()')[0].strip()
-
-    # Filter out Wimbledon stuff for Net 5
-    if title.find('Wimbledon') == -1:
-      programme_url = programme.get('href')
-
-      if title in DIFFERENT:
-        dir.Append(Function(VideoItem(PlayVideo, title=title, thumb=R(CHANNELS[c]['icon'])), url=programme_url))
-      else:
-        dir.Append(Function(DirectoryItem(Episodes, title=title, thumb=R(CHANNELS[c]['icon'])), title=title, url=CHANNELS[c]['base'] + programme_url, c=c))
-
-  if len(dir) == 0:
-    return MessageContainer('Geen items', 'Deze directory is leeg')
-  else:
-    return dir
+	return oc
 
 ####################################################################################################
+def CatagoryListing(kanaal):
+	oc = ObjectContainer(title2=kanaal)
+	
+	oc.add(DirectoryObject(key = Callback(Episode, title='popular', kanaal=kanaal), title=L('Popular'), art=R(CHANNELS[kanaal]['art'])))
+	oc.add(DirectoryObject(key = Callback(Episode, title='0-9', kanaal=kanaal), title=L('StartingWith') + " " + L('09'), art=R(CHANNELS[kanaal]['art'])))
+	oc.add(DirectoryObject(key = Callback(Episode, title='ABC', kanaal=kanaal), title=L('StartingWith') + " " + L('ABC'), art=R(CHANNELS[kanaal]['art'])))
+	oc.add(DirectoryObject(key = Callback(Episode, title='DEF', kanaal=kanaal), title=L('StartingWith') + " " + L('DEF'), art=R(CHANNELS[kanaal]['art'])))
+	oc.add(DirectoryObject(key = Callback(Episode, title='GHI', kanaal=kanaal), title=L('StartingWith') + " " + L('GHI'), art=R(CHANNELS[kanaal]['art'])))
+	oc.add(DirectoryObject(key = Callback(Episode, title='JKL', kanaal=kanaal), title=L('StartingWith') + " " + L('JKL'), art=R(CHANNELS[kanaal]['art'])))
+	oc.add(DirectoryObject(key = Callback(Episode, title='MNO', kanaal=kanaal), title=L('StartingWith') + " " + L('MNO'), art=R(CHANNELS[kanaal]['art'])))
+	oc.add(DirectoryObject(key = Callback(Episode, title='PQR', kanaal=kanaal), title=L('StartingWith') + " " + L('PQR'), art=R(CHANNELS[kanaal]['art'])))
+	oc.add(DirectoryObject(key = Callback(Episode, title='STUV', kanaal=kanaal), title=L('StartingWith') + " " + L('STUV'), art=R(CHANNELS[kanaal]['art'])))
+	oc.add(DirectoryObject(key = Callback(Episode, title='WXYZ', kanaal=kanaal), title=L('StartingWith') + " " + L('WXYZ'), art=R(CHANNELS[kanaal]['art'])))
+	
+	return oc
+	
+###################################################################################################
+def Episode(title, kanaal):
+	oc = ObjectContainer()
 
-def Episodes(sender, title, url, c):
-  dir = MediaContainer(viewGroup='InfoList', title2=title, art=R(CHANNELS[c]['art']))
+	url  = CHANNELS[kanaal]['base'] + PROGRAMLINK + title
+	try:
+		page = HTML.ElementFromURL(url)
+	except:
+		page = ""
+		pass
 
-  for episode in HTML.ElementFromURL(url, errors='ignore').xpath(XPATH_EPISODES):
-    ep_title = episode.xpath('./div[@class="title"]/a/span')[0].text
-    airtime = episode.xpath('./div[@class="airtime"]/a/span')[0].text
-    summary = episode.xpath('./div[@class="text"]/a/span')[0].text
-    thumb = CHANNELS[c]['base'] + episode.xpath('./div[@class="thumb"]/a/img')[0].get('src')
-    ep_url = CHANNELS[c]['base'] + episode.xpath('./div[@class="title"]/a')[0].get('href')
-    dir.Append(Function(VideoItem(PlayVideo, title=ep_title, subtitle=airtime, summary=summary, thumb=Function(GetThumb, url=thumb, alt=CHANNELS[c]['icon'])), url=ep_url))
+	div_main = page.xpath('//div[starts-with(@class, "i iGrid")]')
+	
+	for div in div_main:
+		stream_name = div.xpath('./div/h2/a')[0].text
+		stream_id   = CHANNELS[kanaal]['base'] + div.xpath('./div/h2/a')[0].get('href')
+		url = stream_id + '/videos'
+		
+		#test if the page is actually there
+		try:
+			data = HTTP.Request(url, cacheTime=0).headers
+		except:
+			continue
+		
+		stream_title = stream_name
+		oc.add(DirectoryObject(key = Callback(GetShows, kanaal=kanaal, url=url), title=stream_title, art=R(CHANNELS[kanaal]['art'])))
+		
+	if len(oc) == 0:
+		oc = ObjectContainer(header = L('NoVideo'), message = L('NoClips'))
+		
+	return oc	
+	
+###################################################################################################
+def GetShows(kanaal, url):
+	oc = ObjectContainer()
 
-  next = HTML.ElementFromURL(url, errors='ignore').xpath('//span[@class="next"]/a')
-  if len(next) == 1:
-    next_url = CHANNELS[c]['base'] + next[0].get('href')
-    dir.Append(Function(DirectoryItem(Episodes, title='Meer...', thumb=R(ICON_MORE)), title=title, url=next_url, c=c))
+	page = HTML.ElementFromURL(url)
+	try:
+		style = page.xpath('//section[@class="s ajax"]/header/h1')[0].text
+	except:
+		oc = ObjectContainer(header = "Clip Error", message = "No clips found")
+		style = ""
+		div_main = []
+		
+	if style == 'Clips':
+		div_main = page.xpath('//section[@class="s ajax"]/div/div/div[@class="i iBorder"]')
+	elif style == 'Afleveringen':
+		div_main = page.xpath('//div[@class="i iGuide iGuideSlider"]') 
 
-  if len(dir) == 0:
-    return MessageContainer('Geen items', 'Deze directory is leeg')
-  else:
-    return dir
+	for episodes in div_main:
+		if style == 'Clips':
+			episode_name = str(episodes.xpath('./div/h2/a')[0].text)
+			episode_id =  str(CHANNELS[kanaal]['base'] + episodes.xpath('./a')[0].get('href'))
+			episode_thumb = str(CHANNELS[kanaal]['base'] + episodes.xpath('./a/img')[0].get('src'))
+		elif style == 'Afleveringen':
+			episode_name = str(episodes.xpath('./div/h2')[0].text)
+			episode_id =  str(CHANNELS[kanaal]['base'] + episodes.xpath('./a')[0].get('href'))
+			episode_thumb = str(CHANNELS[kanaal]['base'] + episodes.xpath('./a/img')[0].get('src'))
+		else:
+			continue
 
-####################################################################################################
+		video = VIDEOMATCH.match(episode_id)
+		
+		if video:
+			Log('video found')
+		else:
+			continue
+		
+		oc.add(VideoClipObject(
+			url = episode_id,
+			title = episode_name,
+			thumb=Resource.ContentsOfURLWithFallback(url=episode_thumb, fallback=R(CHANNELS[kanaal]['icon']))	
+		))
+	
+	if len(page.xpath('//div[@class="pager"]')) > 0:
+		count = len(page.xpath('//div[@class="pager"]/ul/li[@class]'))
+		currentpage = page.xpath('//div[@class="pager"]/ul/li[@class="active"]/a')[0].get('href')
+		link, pagenr = currentpage.split('page/')
+		pagenr = int(pagenr)
 
-def PlayVideo(sender, url):
-  content = HTTP.Request(url, cacheTime=0).content
-  content = HTTP.Request(url, cacheTime=0).content # Intentionally!
-
-  # Almost all videos on the website are in Windows Media format (Silverlight), except for a couple program that have Flash videos
-  # The Silverlight stuff uses the Silverlight player on plexapp.com, for the Flash stuff links to the .flv files are looked up
-  vid = re.compile('(http://asx.sbsnet.nl/(.+?)\.(wmv|asx))').findall(content, re.DOTALL)
-  if len(vid) > 0:
-    url = String.Quote(vid[0][0], usePlus=True)
-    return Redirect(WebVideoItem( SILVERLIGHT_PLAYER % (url, '424', '240') ))
-  else:
-    vid = re.compile('(http://playlist.sbsnet.nl/flv/(.+?))"').findall(content, re.DOTALL)
-    if len(vid) > 0:
-      playlist = HTTP.Request(vid[0][0], errors='ignore').content
-      vid = re.compile('<location>(.+?)</location>').findall(playlist, re.DOTALL)
-      if len(vid) > 0:
-        return Redirect(vid[0])
-  return None
-
-####################################################################################################
-
-def GetThumb(url, alt):
-  try:
-    data = HTTP.Request(url, cacheTime=CACHE_1MONTH).content
-    return DataObject(data, 'image/jpeg')
-  except:
-    pass
-  return Redirect(R(alt))
+		if pagenr == count:
+			Log('end of pages reached')
+		else:	
+			pagenr = pagenr + 1
+			pagenr = str(pagenr)
+			url = 	CHANNELS[kanaal]['base'] + 	link + "page/" + pagenr
+			oc.add(DirectoryObject(key=Callback(GetShows, kanaal=kanaal, url=url), title=L('More'), thumb=R(ICON_MORE), art=R(CHANNELS[kanaal]['art'])))
+	
+	return oc
